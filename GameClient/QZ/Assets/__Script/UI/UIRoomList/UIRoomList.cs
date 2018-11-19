fc76c342396a6711f0a4f0c9a369def170125570
QZ <44761342+azk2001@users.noreply.github.com>
2018-11-20 00:33:03 +0800

    构建工程

---
+++ GameClient/QZ/Assets/__Script/UI/UIRoomList/UIRoomList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+class UIRoomList : UIBase
+{
+    public override eUIDepth uiDepth
+    {
+        get
+        {
+            return eUIDepth.ui_system;
+        }
+    }
+
+    public override bool showing
+    {
+        get
+        {
+            return true;
+        }
+    }
+    
+    private DOTweenAnimation tweenAnimation = null;
+
+    public override bool TweenAni
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public DOTweenAnimation TweenAnimation
+    {
+        get
+        {
+            if (tweenAnimation == null)
+            {
+                tweenAnimation = gameObject.GetComponent<DOTweenAnimation>();
+            }
+            return tweenAnimation;
+        }
+    }
+
+    public override void OnAwake(GameObject obj)
+    {
+
+        base.OnAwake(obj);
+        
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+    }
+
+   
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        
+    }
+
+    protected override void OnHintChange(params object[] args)
+    {
+        base.OnHintChange(args);
+    }
+}
